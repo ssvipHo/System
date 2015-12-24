e1b55b4da9e81d437cbfbf7373995dec58148fa2
Bing <song519281000@icloud.com>
2015-12-24 12:14:08 +0000

    the first file

---
+++ App/Common/Conf/config.php
@@ -0,0 +1,16 @@
+<?php
+return array(
+    /* 数据库设置 */
+    'DB_TYPE'               =>  'mysql',     // 数据库类型
+    'DB_HOST'               =>  'localhost', // 服务器地址
+    'DB_NAME'               =>  'system',          // 数据库名
+    'DB_USER'               =>  'root',      // 用户名
+    'DB_PWD'                =>  '1234qwer',          // 密码
+    'DB_PORT'               =>  '3306',        // 端口
+    'DB_PREFIX'             =>  'tp_',    // 数据库表前缀
+    'URL_MODEL'             =>  2,       // URL访问模式,可选参数0、1、2、3,代表以下四种模式：
+//    'TMPL_L_DELIM'          =>  '<{',            // 模板引擎普通标签开始标记
+//    'TMPL_R_DELIM'          =>  '}>',            // 模板引擎普通标签结束标记
+    'SHOW_PAGE_TRACE' =>true,
+
+);
---
+++ App/Runtime/Cache/Home/00e52909bed743ea261af4b6823297ff.php
@@ -0,0 +1,16 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>System</title>
+    <script src=/Public/Js/jquery-2.1.4.js></script>
+</head>
+<body>
+
+<h1>hello world</h1>
+<h3>Working...</h3>
+        <footer>
+
+        </footer>
+    </body>
+</html>
